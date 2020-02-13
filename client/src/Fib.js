9a64d63ec4843bc88f7951a6508dfbb04ea61d93
Michael Connors <michael.connors@gmail.com>
2020-02-13 14:27:12 +0000

    update name

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,10 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    if (Array.isArray(this.state.seenIndexes)) {
+      return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    }
+   return [];
   }
 
   renderValues() {
